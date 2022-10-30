e0352e36878b9098a4fb290cd50dd68fdccc23fb
Agatha <auv2@cornell.edu>
2022-10-30 11:09:01 -0400

    login done

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         glue = "step_definitions",
         stepNotifications = true,
         dryRun = false
-        ,tags = "@Sample"
+        ,tags = "@test1"
 )
 public class CukesRunner {
 }
--- src/test/java/step_definitions/HomeSteps.java
+++ src/test/java/step_definitions/HomeSteps.java
@@ -1,7 +1,13 @@
 package step_definitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import pages.CommonPage;
 import pages.HomePage;
+import utils.BrowserUtils;
 
 public class HomeSteps implements CommonPage {
     HomePage page;
@@ -9,5 +15,28 @@ public class HomeSteps implements CommonPage {
         page = new HomePage();
     }
 
+    @Given("I open URL of login page")
+    public void i_open_url_of_login_page() {
+        BrowserUtils.getDriver();
+    }
 
+    @When("I enter username {string}")
+    public void i_enter_username(String username) {
+        BrowserUtils.sendKeys(page.username, username);
+
+    }
+    @When("I enter password {string}")
+    public void i_enter_password(String password) {
+        BrowserUtils.sendKeys(page.password, password);
+    }
+
+    @And("I click a button {string}")
+    public void iClickAButton(String login) {
+        BrowserUtils.click(page.loginBtn);
+    }
+
+//    @Then("Verify title of the page")
+//    public void verifyTitleOfThePage() {
+//        System.out.println("Test");
+//    }
 }
